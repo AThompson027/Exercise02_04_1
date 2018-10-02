6c7702f44b6f9da10d5b3c46e2bf30f941d0b2f9
Unknown <AThompson027@west-mec.org>
2018-10-02 13:32:47 -0700

    Comments
    Added comments

--- Project02_04_02/Paycheck.php
+++ Project02_04_02/Paycheck.php
@@ -12,20 +12,24 @@
 
     <?php
 
+    // global variables
 $hoursWorked = $_GET["hours"];
 $hourlyWages = $_GET["wage"];
     
+    // output to the submit button with the data the user inputed in the fields
 echo "You entered that you worked: $hoursWorked hours.<br>";
 echo "You entered that your hourly wage is: \$$hourlyWages per hour.<br>";
 
+    //if the hours the user inputed in the field is less than or equal to 40 then the payCheck/output will be the 
+    //hours inputed times the hourly wage inputed. Then it will echo the total/output.
 if ($hoursWorked <= 40){
     $payCheck = ($hoursWorked * $hourlyWages);
 echo "Your Paycheck is: \$$payCheck";
 }
-
+// if the hours the user has inputed are greater than 40 then the payCheck/output will he the hours worked, times the hourly wage, plus the hours worked minus 40 times the hourly wage times 1.5. This will display the total/output with the echo
 if ($hoursWorked > 40){
     $payCheck = ($hoursWorked * $hourlyWages) + (($hoursWorked - 40) * $hourlyWages * 1.5);
-    
+    // the number_format formats the number to 2 decimal places to make it mor eaccurate to dollar currency.
 echo "Your Paycheck is: \$", number_format("$payCheck", 2);
 }
 
